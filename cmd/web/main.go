da9362a3a35496909ed123dfd9ea25b099eb9173
ruanderson1 <ruanderson.gabriel@academico.ufpb.br>
2026-03-04 13:58:46 -0300

    Estrutura inicial do porjeto

---
+++ cmd/web/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
